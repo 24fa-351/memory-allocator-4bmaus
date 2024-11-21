fc64f9d1259be617a01c0cdebb0e8c327da279e4
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-11-21 10:27:46 -0800

    Add files via upload

---
+++ malloc.h
@@ -0,0 +1,14 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <stddef.h>
+
+// Public functions
+void* my_malloc(size_t size);
+void my_free(void* ptr);
+void* my_realloc(void* ptr, size_t size);
+
+// Debugging function
+void print_heap_status(void);
+
+#endif // MALLOC_H
