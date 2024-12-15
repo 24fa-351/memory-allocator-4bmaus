4ca8d3b6ae7e2ce421c92f6a43eb26743451d805
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-12-15 14:06:04 -0800

    Update malloc.h

--- malloc.h
+++ malloc.h
@@ -4,9 +4,9 @@
 #include <stddef.h>
 
 // Public functions
-void* my_malloc(size_t size);
-void my_free(void* ptr);
-void* my_realloc(void* ptr, size_t size);
+void *my_malloc(size_t size);
+void my_free(void *ptr);
+void *my_realloc(void *ptr, size_t size);
 
 // Debugging function
 void print_heap_status(void);
